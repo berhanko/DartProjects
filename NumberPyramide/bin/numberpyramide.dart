ee5b77f2de70c8a8914e0221efc73d15ef51f477
berhanko <berhan.kongel@sodriva.com>
2014-06-30 06:01:09 +0300

    NumberPyramid Project V1.0 Commit

--- NumberPyramide/bin/numberpyramide.dart
+++ NumberPyramide/bin/numberpyramide.dart
@@ -1,3 +1,34 @@
+
+
 void main() {
-  print("Hello, World! You are my bitch!!!");
+  
+  int n =10;
+  int multiplier=1;
+  var mResult, tenPower=1, pSpace=18;
+  String pText='';
+  
+  
+  for (var i = 1; i<n; i++) {
+    
+    //calculate x-pos on pyramid & print spaces
+    for (var j = 1; j<pSpace; j++){
+      pText='$pText' + ' ';
+    }
+    
+    mResult = multiplier * multiplier;
+    pText = '$pText' + '$multiplier x $multiplier = $mResult';
+    print('$pText');
+    //print('$multiplier x $multiplier = $mResult');
+    
+    //calculate next multiplier
+    tenPower=tenPower*10;
+    multiplier=multiplier + tenPower;
+
+    //re-initialize pyramid positions
+    pSpace=pSpace-2;
+    pText='';
+     
+    
+  }
+  
 }
